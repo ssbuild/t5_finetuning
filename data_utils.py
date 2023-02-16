0143d611f6c26900e011d4a440c8dc91154387d1
ssbuild <462304@qq.cn>
2023-02-16 11:30:48 +0800

    chat finetuning
    Signed-off-by: ssbuild <462304@qq.cn>

---
+++ data_utils.py
@@ -0,0 +1,238 @@
+# @Time    : 2023/1/22 16:22
+# @Author  : tk
+# @FileName: data_utils.py
+
+
+import copy
+import json
+import os
+import random
+import typing
+
+import numpy as np
+import torch
+from deep_training.data_helper import DataHelper, ModelArguments, TrainingArguments, DataArguments
+from deep_training.utils.func import is_chinese_char
+from fastdatasets.record import load_dataset as Loader, RECORD, WriterObject, gfile
+from tqdm import tqdm
+from transformers import T5Tokenizer, HfArgumentParser
+
+train_info_args = {
+    'devices': 1,
+    'data_backend': 'record',
+    'model_type': 't5',
+    # 预训练模型路径 , 从0训练，则置空
+    'model_name_or_path': '/data/nlp/pre_models/torch/t5/ChatYuan-large-v1',
+    'tokenizer_name': '/data/nlp/pre_models/torch/t5/ChatYuan-large-v1',
+    'config_name': '/data/nlp/pre_models/torch/t5/ChatYuan-large-v1/config.json',
+    'convert_onnx': False, # 转换onnx模型
+    'do_train': True, 
+    'train_file':  [ '/data/nlp/nlp_train_data/chatyuan/finetune_train_examples.json'],
+    'max_epochs': 3,
+    'train_batch_size': 6,
+    'eval_batch_size': 2,
+    'test_batch_size': 2,
+    'learning_rate': 5e-5,
+    'adam_epsilon': 1e-8,
+    'gradient_accumulation_steps': 1,
+    'max_grad_norm': 1.0,
+    'weight_decay': 0,
+    'warmup_steps': 0,
+    'output_dir': './output',
+    'max_seq_length': 512,
+    'max_target_length': 100  # 预测最大长度
+}
+
+data_conf = {
+    'stride': 50, #滑动窗口 ， 数据多则相应增大，否则减小
+    'count_per_group': 1, #大规模训练可以提高 ,#chatyuan 应该是1
+}
+def preprocess(text):
+  text = text.replace("\n", "\\n").replace("\t", "\\t")
+  return text
+
+def postprocess(text):
+  return text.replace("\\n", "\n").replace("\\t", "\t")
+
+
+class NN_DataHelper(DataHelper):
+    index = 1
+
+    def on_data_ready(self):
+        self.index = -1
+
+    # 切分词
+    def on_data_process(self, data: typing.Any, mode: str):
+        self.index += 1
+
+        tokenizer: T5Tokenizer
+        max_seq_length = self.max_seq_length_dict[mode]
+        tokenizer = self.tokenizer
+
+        stride = data_conf['stride']
+        COUNT_PER_GROUP = data_conf['count_per_group']
+
+        sub_list = data
+        input_ids = []
+
+        for idx, paragraphs in enumerate(sub_list):
+            if COUNT_PER_GROUP > 1:
+                text = paragraphs + '<extra_id_0>'
+            else:
+                text = paragraphs
+            o = tokenizer.encode_plus(text=text, truncation=True,
+                                      return_attention_mask=False,
+                                      return_token_type_ids=False)
+            if len(o['input_ids']) <= 3:
+                continue
+            input_ids += o['input_ids'][:-1]
+            if idx != len(sub_list) - 1:
+                input_ids += [tokenizer.eos_token_id]
+
+        pos = 0
+        ds = []
+        while pos < len(input_ids):
+            input_ids_ = input_ids[pos: pos + max_seq_length - 2] + [tokenizer.eos_token_id]
+            pos += stride
+
+            if len(input_ids_) <= 5:
+                continue
+            seqlen = np.asarray(len(input_ids_), dtype=np.int32)
+            pad_len = max_seq_length - seqlen
+            input_ids_ = np.asarray(input_ids_, dtype=np.int32)
+            if pad_len:
+                pad_val = tokenizer.pad_token_id
+                input_ids_ = np.pad(input_ids_, (0, pad_len), 'constant', constant_values=(pad_val, pad_val))
+            d = {
+                'input_ids': input_ids_,
+                'seqlen': seqlen
+            }
+            ds.append(d)
+        if self.index < 3:
+            print(ds[0])
+        return ds
+
+
+
+    # 读取文件
+    def on_get_corpus(self, files: typing.List, mode: str):
+        D = []
+        #{"input": "我可以用以下的句子：“花呗在什么时间段可以用”，来替换这个句子：“什么时候用了花贝”，并且它们有相同的意思？。选项：是的，不是。答案：", "target": "不是", "type": "classify"}
+        COUNT_PER_GROUP = data_conf['count_per_group']
+
+        sub = []
+        for file in files:
+            with open(file,mode='r',encoding='utf-8',newline='\n') as f:
+                lines = f.readlines()
+
+            for i,line in enumerate(lines):
+                jd = json.loads(line)
+                if not jd:
+                    continue
+                paragraph = jd['paragraph']
+                if i < 10:
+                    print(paragraph)
+                texts = ''
+                for text in paragraph:
+                    texts += preprocess(text + '\n')
+                sub.append(texts)
+                if len(sub) >= COUNT_PER_GROUP:
+                    D.append(copy.deepcopy(sub))
+                    sub.clear()
+        if sub:
+            D.append(copy.deepcopy(sub))
+        return D
+
+    def collate_fn(self, batch):
+        self.tokenizer: T5Tokenizer
+        o = {}
+        for i, b in enumerate(batch):
+            if i == 0:
+                for k in b:
+                    o[k] = [torch.tensor(b[k])]
+            else:
+                for k in b:
+                    o[k].append(torch.tensor(b[k]))
+        for k in o:
+            o[k] = torch.stack(o[k])
+
+        seqlens = o.pop('seqlen')
+        max_len = torch.max(seqlens).numpy().tolist()
+
+        bs = len(batch)
+        pad_token_id = self.tokenizer.pad_token_id
+        eos_token_id = self.tokenizer.eos_token_id
+
+
+        input_ids = torch.full((bs, max_len), pad_token_id, dtype=torch.long)
+        attention_mask = torch.zeros(size=(bs, max_len), dtype=torch.long)
+        decoder_input_ids = torch.full((bs, max_len), pad_token_id, dtype=torch.long)
+        decoder_attention_mask = torch.zeros(size=(bs, max_len), dtype=torch.long)
+        labels = torch.full((bs, max_len), -100, dtype=torch.long)
+
+        a_maxlen, b_maxlen = 0, 0
+        raw_input_ids = o.pop('input_ids')
+
+        for (seqlen, ids, a_ids, a_mask, b_ids, b_mask, label) in zip(seqlens, raw_input_ids, input_ids, attention_mask,
+                                                                      decoder_input_ids, decoder_attention_mask,
+                                                                      labels):
+            seqlen = seqlen.squeeze(-1).numpy().tolist()
+            s = np.random.randint(1, seqlen - 1, dtype=np.int32).tolist()
+            a_ids[:s] = ids[:s]
+            a_ids[s] = eos_token_id
+            a_mask[:s + 1] = 1
+
+            b_ids[:seqlen - s] = ids[s:seqlen]
+            b_mask[:seqlen - s] = 1
+            label[:seqlen - s-1] = b_ids[1:seqlen - s]
+            a_maxlen = max(a_maxlen, s + 1)
+            b_maxlen = max(b_maxlen, seqlen - s)
+
+        o['input_ids'] = input_ids[:, :a_maxlen]
+        o['attention_mask'] = attention_mask[:, :a_maxlen]
+        o['decoder_input_ids'] = decoder_input_ids[:, :b_maxlen]
+        o['decoder_attention_mask'] = decoder_attention_mask[:, :b_maxlen]
+        o['labels'] = labels[:, :b_maxlen]
+        return o
+
+
+if __name__ == '__main__':
+
+    parser = HfArgumentParser((ModelArguments, TrainingArguments, DataArguments))
+    model_args, training_args, data_args = parser.parse_dict(train_info_args)
+
+    dataHelper = NN_DataHelper(model_args, training_args, data_args)
+    tokenizer, config, label2id, id2label = dataHelper.load_tokenizer_and_config()
+    config.decoder_start_token_id = tokenizer.cls_token_id
+    # 缓存数据集
+    # 检测是否存在 output/dataset_0-train.record ，不存在则制作数据集
+    if data_args.do_train:
+        dataHelper.make_dataset_with_args(data_args.train_file, shuffle=True,mode='train')
+    if data_args.do_eval:
+        dataHelper.make_dataset_with_args(data_args.eval_file, shuffle=False,mode='eval')
+    if data_args.do_test:
+        dataHelper.make_dataset_with_args(data_args.test_file, shuffle=False,mode='test')
+
+
+    def shuffle_records(record_filenames, outfile, compression_type='GZIP'):
+        print('shuffle_records record...')
+        options = RECORD.TFRecordOptions(compression_type=compression_type)
+        dataset_reader = Loader.RandomDataset(record_filenames, options=options, with_share_memory=True)
+        data_size = len(dataset_reader)
+        all_example = []
+        for i in tqdm(range(data_size), desc='load records'):
+            serialized = dataset_reader[i]
+            all_example.append(serialized)
+        dataset_reader.close()
+
+        shuffle_idx = list(range(data_size))
+        random.shuffle(shuffle_idx)
+        writer = WriterObject(outfile, options=options)
+        for i in tqdm(shuffle_idx, desc='shuffle record'):
+            example = all_example[i]
+            writer.write(example)
+        writer.close()
+
+
+    # 再次打乱数据
+    shuffle_records(dataHelper.train_files, dataHelper.train_files[0])
---
+++ task_chat_t5.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+#reference: https://github.com/clue-ai/PromptCLUE/blob/main/Fine_tuning_PyTorch.ipynb
+
+import numpy as np
+import torch
+import transformers
+from deep_training.data_helper import ModelArguments, DataArguments, TrainingArguments
+from deep_training.nlp.models.transformer import TransformerForSeq2SeqLM
+from deep_training.utils.trainer import SimpleModelCheckpoint
+from pytorch_lightning import Trainer
+from torch.utils.data import DataLoader, IterableDataset
+from transformers import HfArgumentParser, T5Tokenizer
+
+from data_utils import NN_DataHelper, train_info_args,preprocess,postprocess
+
+
+class MyTransformer(TransformerForSeq2SeqLM, with_pl=True):
+    def __init__(self, *args, **kwargs):
+        super(MyTransformer, self).__init__(*args, **kwargs)
+
+
+class MySimpleModelCheckpoint(SimpleModelCheckpoint):
+    def __init__(self, *args, **kwargs):
+        super(MySimpleModelCheckpoint, self).__init__(*args, **kwargs)
+        self.weight_file = './best.pt'
+
+    @staticmethod
+    def generate_text(pl_module: MyTransformer, prefix, tokenizer, max_target_length, device=0):
+        device = torch.device('cuda:{}'.format(device))
+        config = pl_module.config
+
+        model: transformers.T5ForConditionalGeneration
+        model = pl_module.backbone.model
+
+        # 简易测试生成
+        o = tokenizer.encode_plus(preprocess(prefix), truncation=True, max_length=512, return_attention_mask=False,return_token_type_ids=False)
+        input_ids= [o['input_ids']]
+        input_ids = torch.tensor(input_ids, dtype=torch.int32,device=device)
+
+        logits = model.generate(input_ids,max_length=max_target_length,bos_token_id=config.decoder_start_token_id,
+                                pad_token_id=config.pad_token_id,
+                                eos_token_id=config.eos_token_id)
+
+
+        out_text = tokenizer.decode(logits[0], skip_special_tokens=True)
+        out_text = postprocess(out_text)
+        return out_text
+
+    def on_save_model(
+            self, trainer: "pl.Trainer", pl_module: "pl.LightningModule"
+    ) -> None:
+        # 保存权重
+        super(MySimpleModelCheckpoint, self).on_save_model(trainer, pl_module)
+        prefixs = ['用户：帮我写一个请假条，我因为新冠不舒服，需要请假3天，请领导批准'
+                  '用户：你能干什么',
+                  '用户：写一个诗歌，关于冬天',
+                   ]
+        print('*' * 30)
+        device = trainer.global_rank
+        self.tokenizer: T5Tokenizer
+        tokenizer = self.tokenizer
+        data_args = self.data_args
+        for prefix in prefixs:
+            output = MySimpleModelCheckpoint.generate_text(pl_module, prefix, tokenizer,
+                                                           data_args.max_target_length, device=device)
+            print('input', prefix)
+            print('output', output)
+            print()
+
+
+
+if __name__ == '__main__':
+    parser = HfArgumentParser((ModelArguments, TrainingArguments, DataArguments))
+    model_args, training_args, data_args = parser.parse_dict(train_info_args)
+    # 保存最小loss模型
+    checkpoint_callback = MySimpleModelCheckpoint(monitor="loss",
+                                                  every_n_epochs = 1,
+                                                  every_n_train_steps=2000 // training_args.gradient_accumulation_steps)
+    trainer = Trainer(
+        callbacks=[checkpoint_callback],
+        max_epochs=training_args.max_epochs,
+        max_steps=training_args.max_steps,
+        accelerator="gpu",replace_sampler_ddp=False,
+        devices=data_args.devices,
+        enable_progress_bar=True,
+        default_root_dir=data_args.output_dir,
+        gradient_clip_val=training_args.max_grad_norm,
+        accumulate_grad_batches=training_args.gradient_accumulation_steps,
+        num_sanity_val_steps=0,
+        strategy='ddp' if torch.cuda.device_count() > 1 else None,
+    )
+
+    dataHelper = NN_DataHelper(model_args, training_args, data_args)
+    tokenizer, config, label2id, id2label = dataHelper.load_tokenizer_and_config()
+
+    # 额外参数
+    checkpoint_callback.tokenizer = tokenizer
+    checkpoint_callback.data_args = data_args
+
+    # 缓存数据集
+    if data_args.do_train:
+        dataHelper.make_dataset_with_args(data_args.train_file,shuffle=True,mode='train')
+    if data_args.do_eval:
+        dataHelper.make_dataset_with_args(data_args.eval_file, mode='eval')
+    if data_args.do_test:
+        dataHelper.make_dataset_with_args(data_args.test_file,mode='test')
+
+
+    model = MyTransformer(config=config, model_args=model_args, training_args=training_args)
+
+    if not data_args.convert_onnx:
+        train_datasets = dataHelper.load_random_sampler(dataHelper.train_files,
+                                                        batch_size=training_args.train_batch_size,
+                                                        collate_fn=dataHelper.collate_fn,
+                                                        shuffle=True,
+                                                        infinite=True,
+                                                        with_load_memory=True,
+                                                        num_processes=trainer.world_size,
+                                                        process_index=trainer.global_rank)
+        if train_datasets is not None:
+            trainer.fit(model, train_dataloaders=train_datasets)
+        else:
+            eval_datasets = dataHelper.load_sequential_sampler(dataHelper.eval_files,batch_size=training_args.eval_batch_size,collate_fn=dataHelper.collate_fn)
+            test_datasets = dataHelper.load_sequential_sampler(dataHelper.test_files,batch_size=training_args.test_batch_size,collate_fn=dataHelper.collate_fn)
+            if eval_datasets is not None:
+                trainer.validate(model, dataloaders=eval_datasets, ckpt_path='./best.pt')
+
+            if test_datasets is not None:
+                trainer.test(model, dataloaders=test_datasets, ckpt_path='best.pt')
+    else:
+        # 加载权重
+        model = MyTransformer.load_from_checkpoint('./best.pt', config=config,
+                                                   model_args=model_args,
+                                                   training_args=training_args)
+        model.convert_to_onnx('./best.onnx')
