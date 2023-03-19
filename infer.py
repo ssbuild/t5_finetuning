7494da23efbb33bad5e00aaae3dc28d2a5137db6
tk <462304@qq.com>
2023-03-19 18:33:28 +0800

    update
    Signed-off-by: tk <462304@qq.com>

--- infer.py
+++ infer.py
@@ -44,6 +44,8 @@ if __name__ == '__main__':
     parser = HfArgumentParser((ModelArguments, TrainingArguments, DataArguments, LoraArguments))
     model_args, training_args, data_args, lora_args = parser.parse_dict(train_info_args)
 
+
+
     dataHelper = NN_DataHelper(model_args, training_args, data_args)
     tokenizer, config, _,_= dataHelper.load_tokenizer_and_config()
 
