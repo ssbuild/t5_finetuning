b8c1c9b465bd6233fc888aa9a1ea74a6cb6b1a44
ssbuild <462304@qq.cn>
2023-08-14 13:54:34 +0800

    update
    Signed-off-by: ssbuild <462304@qq.cn>

--- data_utils.py
+++ data_utils.py
@@ -127,46 +127,79 @@ class NN_DataHelper(DataHelper):
         ds = self.get_feature(example,stride,tokenizer,max_seq_length)
         return ds
 
-    # {
-    #     "id": 0, "paragraph": [
-    #     # 一轮会话
-    #     {
-    #         "q": "从南京到上海的路线",
-    #         "a": [
-    #             "你好，南京到上海的路线如下：",
-    #             "1. 南京到上海，可以乘坐南京地铁1号线，在南京站乘坐轨道交通1号线。",
-    #             "2. 南京到浦东机场，可以搭乘上海地铁1号，在陆家嘴站乘坐地铁1线，在浦东国际机场站乘坐机场快线，前往上海浦东国际机场。",
-    #             "3. 上海到南京，可以换乘上海地铁2号线，从南京站换乘地铁2线，再从南京南站换乘地铁1路，然后到达上海站"
-    #         ]
-    #     }
-    #     # 二轮....
-    # ]
-    # }
+    def _get_paragraph(self, lines):
+        D = []
+        for line_id, line in enumerate(lines):
+            jd = json.loads(line)
+            if not jd:
+                continue
+            paragraph = jd['paragraph']
+            if line_id < 10:
+                print(paragraph)
+
+            prefix = jd.get('p', '')
+            paragraph = [(preprocess(session['q']),
+                          preprocess('\n'.join(session['a'])) if isinstance(session['a'], list) else preprocess(
+                              session['a']))
+                         for session in paragraph]
+            sub = []
+            # 自行做模板
+            for (q, a) in paragraph:
+                assert len(a), ValueError('answer cannot empty')
+                sub.append((preprocess('用户：' + q + '小元：' ), a))
+            D.append((prefix, copy.deepcopy(sub)))
+            sub.clear()
+        return D
+
+    def _get_messages(self, lines):
+        D = []
+        for line_id, line in enumerate(lines):
+            jd = json.loads(line)
+            if not jd:
+                continue
+            conversations = jd['conversations']
+            if line_id < 10:
+                print(conversations)
+
+            paragraph = []
+            prefix = ''
+            pair = [None, None]
+            for m in conversations:
+                if m["from"] == 'user':
+                    pair[0] = preprocess(m["value"])
+                elif m["from"] == 'assistant':
+                    pair[1] = preprocess(m["value"])
+                elif m["from"] == 'system':
+                    prefix = preprocess(m["value"])
+                if pair[0] is not None and pair[1] is not None:
+                    paragraph.append(tuple(pair))
+                    pair[0], pair[1] = None, None
+
+            sub = []
+            # 自行做模板
+            for (q, a) in paragraph:
+                assert len(a), ValueError('answer cannot empty')
+                sub.append((preprocess('用户：' + q + '小元：' ), a))
+            D.append((prefix, copy.deepcopy(sub)))
+            sub.clear()
+        return D
+
     # 读取文件
     def on_get_corpus(self, files: typing.List, mode: str):
         D = []
         for file in files:
-            with open(file,mode='r',encoding='utf-8',newline='\n') as f:
+            with open(file, mode='r', encoding='utf-8', newline='\n') as f:
                 lines = f.readlines()
-
-            for i,line in enumerate(lines):
-                jd = json.loads(line)
-                if not jd:
-                    continue
-                paragraph = jd['paragraph']
-                if i < 10:
-                    print(paragraph)
-                sub = []
-                for session in paragraph:
-                    q = session['q']
-                    answers_list = session['a']
-                    q = preprocess('用户：' + q + '小元：' )
-                    answer = preprocess('\n'.join(answers_list)) if isinstance(answers_list,list) else answers_list
-                    sub.append((q,answer))
-                D.append(copy.deepcopy(sub))
-                sub.clear()
-
+            is_new = False
+            if len(lines) > 0:
+                is_new = 'conversations' in json.loads(lines[0])
+            if is_new:
+                D.extend(self._get_messages(lines))
+            else:
+                D.extend(self._get_paragraph(lines))
         return D
+
+
 
     def collate_fn_stride(self, batch):
         self.tokenizer: T5Tokenizer
